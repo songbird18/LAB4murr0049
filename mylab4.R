d4826ddc15350231805cafc30cf9f460e5561ce7
Chelsea Murray <chelsea.g.murray-1@ou.edu>
2022-09-16 23:56:07 -0500

    Finish task 6 before submission deadline

--- mylab4.R
+++ mylab4.R
@@ -44,5 +44,30 @@ trendscatter(height.fit,height.res)
 plot(spruce.lm, which =1)
 
 
-# Check normality
+# Check normality with a QQ plot + Shapiro-Wilk data
 normcheck(spruce.lm,shapiro.wilk = TRUE)
+
+
+# Fit a quadratic curve to the plot of Height vs. BHDiameter
+quad.lm=lm(Height~BHDiameter + I(BHDiameter^2),data=spruce.df)
+
+# Plot spruce height predictions
+plot(Height~BHDiameter,bg="Blue",pch=21,cex=1.2,
+     ylim=c(0,max(Height)),xlim=c(0,max(BHDiameter)),
+     main="Spruce height prediction",data=spruce.df)
+
+# Declare a function to calculate the quadratic polynomial
+myplot=function(x){
+  quad.lm$coef[1] +quad.lm$coef[2]*x  + quad.lm$coef[3]*x^2
+}
+
+
+# Plot the quadratic curve over the existing scatter plot
+curve(myplot, lwd=2, col="steelblue",add=TRUE)
+
+
+# similar to code with 'spruce' variables above
+quad.fit=fitted(quad.lm)
+quad.res=residuals(quad.lm)
+plot(quad.lm, which =1)
+normcheck(quad.lm,shapiro.wilk = TRUE)
